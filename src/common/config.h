be062ed7b8c5a811e9cf95cbeddbff4374e20d8b
Juraj Michálek <juraj.michalek@espressif.com>
2024-09-05 14:41:26 +0200

    creating new forrk to implement missing functionality

--- src/common/config.h
+++ src/common/config.h
@@ -175,8 +175,10 @@
 #	define LOVE_ENABLE_LUAHTTPS
 #endif
 
+#define LOVE_ESP_IDF
+
 // Check we have a sane configuration
-#if !defined(LOVE_WINDOWS) && !defined(LOVE_LINUX) && !defined(LOVE_IOS) && !defined(LOVE_MACOS) && !defined(LOVE_ANDROID)
+#if !defined(LOVE_WINDOWS) && !defined(LOVE_LINUX) && !defined(LOVE_IOS) && !defined(LOVE_MACOS) && !defined(LOVE_ANDROID) && !defined(LOVE_ESP_IDF)
 #	error Could not detect target platform
 #endif
 #if !defined(LOVE_LITTLE_ENDIAN) && !defined(LOVE_BIG_ENDIAN)
